669ca8a269176ceb55058d7045e8c0b68ce41627
cm <cmazs9@163.com>
2019-05-27 17:39:03 +0800

    工具类以及冒泡排序
    工具类以及冒泡排序

---
+++ Algorithm/src/course/charper4/BubbleSort.java
@@ -0,0 +1,82 @@
+package course.charper4;
+
+import course.util.ArrayUtils;
+
+/**
+ * 冒泡排序
+ * @author cm
+ *
+ */
+public class BubbleSort {
+	
+	/**
+	 * 冒泡原始算法
+	 * @param array
+	 */
+	public static void sort(int[] array){
+		for(int i = array.length - 1; i > 0; i --){
+			for(int j = 0; j < i; j ++){
+				if(array[j] > array[j + 1]){
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 优化，当某一次遍历没有交换元素，则证明数组已经有序，则跳出循环
+	 * @param array
+	 */
+	public static void sort2(int[] array){
+		for(int i = array.length - 1; i > 0; i --){
+			boolean flag = true;
+			for(int j = 0; j < i; j ++){
+				if(array[j] > array[j + 1]){
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					flag = false;
+				}
+			}
+			if(flag){
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * 再次改进，如果某个下标之后已经有序，比如3,4,2,1,5,6,7,8
+	 * 5之后所有的元素均大于5之前，则3,4,2,1没必要每一次跟5,6,7,8比较
+	 * @param array
+	 */
+	public static void sort3(int[] array){
+		int lastChangeIndex = array.length - 1;
+		for(int i = array.length - 1; i > 0; i --){
+			boolean flag = true;
+			int borderIndex = i;
+			for(int j = 0; j < lastChangeIndex; j ++){
+				if(array[j] > array[j + 1]){
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					flag = false;
+					borderIndex = j;
+				}
+			}
+			lastChangeIndex = borderIndex;
+			if(flag){
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = ArrayUtils.createArray(10);
+		ArrayUtils.display(array);
+		sort3(array);
+		ArrayUtils.display(array);
+	}
+
+}
---
+++ Algorithm/src/course/util/ArrayUtils.java
@@ -0,0 +1,22 @@
+package course.util;
+
+public class ArrayUtils {
+	
+	public static void display(int[] array){
+		String result = "[";
+		for(int i: array){
+			result += i + ",";
+		}
+		result = result.indexOf(",") > -1? result.substring(0, result.lastIndexOf(",")) + "]": result + "]";
+		System.out.println(result);
+	}
+	
+	public static int[] createArray(int length){
+		int[] array = new int[length];
+		for(int i = 0; i < length; i ++){
+			array[i] = (int) (Math.random() * 100);
+		}
+		return array;
+	}
+
+}
