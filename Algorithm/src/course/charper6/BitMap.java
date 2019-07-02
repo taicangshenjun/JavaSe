70fb0223afd6fcd1d50ba6a92ce98744983b95c4
cm <cmazs9@163.com>
2019-07-02 14:42:08 +0800

    位图算法
    位图算法

--- Algorithm/src/course/charper6/BitMap.java
+++ Algorithm/src/course/charper6/BitMap.java
@@ -7,6 +7,7 @@ package course.charper6;
  */
 public class BitMap {
 
+	//一个long数据结构可存储64位
 	private long[] words;
 	
 	private int size;
@@ -16,14 +17,24 @@ public class BitMap {
 		this.words = new long[(getWordIndex(size - 1) + 1)];
 	}
 	
+	/**
+	 * 将对应的位设置成为1
+	 * @param bitIndex
+	 * @return
+	 */
 	public boolean getBit(int bitIndex) {
 		if(bitIndex < 0 || bitIndex > size - 1) {
 			throw new IndexOutOfBoundsException("超过Bitmap有效范围！");
 		}
 		int wordIndex = getWordIndex(bitIndex);
+		//java中long一共有64位，若左移126位，则实际左移126-64位
 		return (words[wordIndex] & (1L << bitIndex)) != 0;
 	}
 	
+	/**
+	 * 判断某一位的状态，1是true，0是false
+	 * @param bitIndex
+	 */
 	public void setBit(int bitIndex) {
 		if(bitIndex < 0 || bitIndex > size - 1) {
 			throw new IndexOutOfBoundsException("超过Bitmap有效范围！");
@@ -32,6 +43,11 @@ public class BitMap {
 		words[wordIndex] |= (1L << bitIndex);
 	}
 	
+	/**
+	 * 定位Bitmap所在的word在数组中的下标
+	 * @param bitIndex
+	 * @return
+	 */
 	public int getWordIndex(int bitIndex) {
 		return bitIndex >> 6;
 	}
